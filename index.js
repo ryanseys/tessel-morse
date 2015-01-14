182ca2e185801d1fbe480e9ce5255fde884400e7
Swaagie <info@martijnswaagman.nl>
2015-01-14 23:19:32 +0100

    [minor] change to make functions configurable

--- index.js
+++ index.js
@@ -13,47 +13,52 @@ function ledOff() {
   LED.output(0);
 }
 
-var q = async.queue(function(c, callback) {
-  var onDuration;
-  var offDuration;
-  if (c === WORD_DELIMITER) {
-    onDuration = 0;
-    offDuration = 7;
-  } else if (c === '.') {
-    onDuration = 1;
-    offDuration = 1;
-  } else if (c === '-') {
-    onDuration = 3;
-    offDuration = 1;
-  } else if (c === ' ') {
-    onDuration = 0;
-    offDuration = 3;
-  }
-
-  if (onDuration > 0) {
-    ledOn();
-  }
-  setTimeout(function() {
-    ledOff();
-    setTimeout(function() {
-      callback();
-    }, offDuration * DOT_DURATION);
-  }, onDuration * DOT_DURATION);
-}, 1);
-
 function morse_blink(str, options) {
   options = options || {};
+  options.on = options.on || ledOn;
+  options.off = options.off || ledOff;
+
   if (typeof options.duration === 'number' && !isNaN(options.duration)) {
     DOT_DURATION = options.duration;
   }
   if (typeof options.led === 'number' && !isNaN(options.led)) {
     LED = tessel.led[options.led] || LED;
   }
+
+  var q = async.queue(function(c, callback) {
+    var onDuration;
+    var offDuration;
+    if (c === WORD_DELIMITER) {
+      onDuration = 0;
+      offDuration = 7;
+    } else if (c === '.') {
+      onDuration = 1;
+      offDuration = 1;
+    } else if (c === '-') {
+      onDuration = 3;
+      offDuration = 1;
+    } else if (c === ' ') {
+      onDuration = 0;
+      offDuration = 3;
+    }
+
+    if (onDuration > 0) {
+      options.on();
+    }
+    setTimeout(function() {
+      options.off();
+      setTimeout(function() {
+        callback();
+      }, offDuration * DOT_DURATION);
+    }, onDuration * DOT_DURATION);
+  }, 1);
+
   var morseStr = morse.encode(str.split(' ')).join(WORD_DELIMITER);
   var arr = morseStr.split('');
   for (var i = 0, l = arr.length; i < l; i++) {
     q.push(arr[i]);
   }
+
   return morse.encode(str);
 }
 
